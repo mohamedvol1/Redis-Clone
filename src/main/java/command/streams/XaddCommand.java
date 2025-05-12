9dcdb4ec606003be2b54a98da891f89e215ebb17
mohamed khaled <70990780+mohamedvol1@users.noreply.github.com>
2025-05-13 01:37:50 +0300

    Validate Id specified ID on XADD command

--- src/main/java/command/streams/XaddCommand.java
+++ src/main/java/command/streams/XaddCommand.java
@@ -11,23 +11,30 @@ import java.util.Map;
 
 public class XaddCommand implements Command {
     @Override
-    public void execute (SocketChannel client, List<String> commandParts, DataStore store) throws Exception {
+    public void execute(SocketChannel client, List<String> commandParts, DataStore store) throws Exception {
         if (commandParts.size() < 4 || (commandParts.size() - 3) % 2 != 0) {
             throw new Exception("ERR wrong number of arguments for 'XADD' command");
         }
 
-        String key = commandParts.get(1);
-        String id = commandParts.get(2);
+        try {
+            String key = commandParts.get(1);
+            String id = commandParts.get(2);
 
-        Map<String, String> fields = new HashMap<>();
-        for (int i = 3; i < commandParts.size(); i += 2) {
-            fields.put(commandParts.get(i), commandParts.get(i + 1));
+            Map<String, String> fields = new HashMap<>();
+            for (int i = 3; i < commandParts.size(); i += 2) {
+                fields.put(commandParts.get(i), commandParts.get(i + 1));
+            }
+
+            // this to be modified to let method add entries to without specifying id (use overloading)
+            String streamEntryId = store.addToStream(key, id, fields);
+            String response = "$" + streamEntryId.length() + "\r\n" + streamEntryId + "\r\n";
+            client.write(ByteBuffer.wrap(response.getBytes()));
+        } catch (Exception e) {
+            String errorMsg = "-" + e.getMessage() + "\r\n";
+            client.write(ByteBuffer.wrap((errorMsg).getBytes()));
         }
 
-        // this to be modified to let method add entries to without specifying id (use overloading)
-        String streamEntryId = store.addToStream(key, id, fields);
-        String response = "$" + streamEntryId.length() + "\r\n" + streamEntryId + "\r\n";
-        client.write(ByteBuffer.wrap(response.getBytes()));
+
     }
 
     @Override
--- src/main/java/streams/Stream.java
+++ src/main/java/streams/Stream.java
@@ -11,11 +11,55 @@ public class Stream {
     }
 
     public String addEntry(StreamEntry entry) {
+        validateEntryId(entry.id());
         entries.put(entry.id(), entry);
         return entry.id();
     }
 
     public Map<String, StreamEntry> getEntries() {
         return entries;
+    }
+
+    public String getLastEntryId() {
+        if (entries.isEmpty()) {
+            return "0-0";
+        }
+
+        return entries.keySet().stream().skip(entries.size() - 1).findFirst().orElse("0-0");
+    }
+
+    private long[] parseEntryId(String entryId) {
+        String[] parts = entryId.split("-");
+        try {
+            return new long[]{Long.parseLong(parts[0]), Long.parseLong(parts[1])};
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("ERR Invalid stream ID specified: " + entryId);
+        }
+    }
+
+    private int compareIDs(String id1, String id2) {
+        long[] id1Parts = parseEntryId(id1);
+        long[] id2Parts = parseEntryId(id2);
+
+        // compare time first if different
+        if (id1Parts[0] != id2Parts[0]) {
+            return Long.compare(id1Parts[0], id2Parts[0]);
+        }
+
+        // if they are equal, compare sequence numbers
+        return Long.compare(id1Parts[1], id2Parts[1]);
+    }
+
+    public void validateEntryId(String id) {
+
+        // id must be bigger than 0-0
+        if (compareIDs(id, "0-1") < 0) {
+            throw new IllegalArgumentException("ERR The ID specified in XADD must be greater than 0-0");
+        }
+
+        String lastId = getLastEntryId();
+        if (compareIDs(id, lastId) <= 0) {
+            throw new IllegalArgumentException("ERR The ID specified in XADD is equal or smaller than the target stream top item");
+        }
     }
 }
